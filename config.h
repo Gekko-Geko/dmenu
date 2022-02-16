e954c430175d58d66acf1989848c9a57652e61a9
rfticar <rfticar@gmail.com>
2022-02-16 20:00:40 +0000

    update

--- config.h
+++ config.h
@@ -1,18 +1,20 @@
 /* See LICENSE file for copyright and license details. */
+
+#include </home/geko/.cache/wal/colors-wal-dmenu.h>
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=18"
+	"Hack:size=21"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#ff5722" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { "#bbbbbb", "#222222" },
+// 	[SchemeSel] = { "#eeeeee", "#ff5722" },
+// 	[SchemeOut] = { "#000000", "#00ffff" },
+// };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
@@ -21,3 +23,4 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
